3781f595f61510f74f698faa9ab07732e6312b13
Rodrigo Ponce de León <rodrigo@wyeworks.com>
2015-11-12 12:59:14 -0300

    Comment out unused plugins

--- vim/add-submodules-script.sh
+++ vim/add-submodules-script.sh
@@ -5,42 +5,42 @@ git submodule add https://github.com/tpope/vim-pathogen.git core/pathogen
 git submodule add https://github.com/lukaszb/vim-web-indent.git indent/web-indent
 
 # Plugins
-git submodule add https://github.com/tpope/vim-abolish.git plugins/abolish
-git submodule add https://github.com/mileszs/ack.vim.git plugins/ack
+# git submodule add https://github.com/tpope/vim-abolish.git plugins/abolish
+# git submodule add https://github.com/mileszs/ack.vim.git plugins/ack
 git submodule add https://github.com/bling/vim-airline plugins/vim-airline
 git submodule add https://github.com/tpope/vim-bundler.git plugins/bundler
 git submodule add https://github.com/tpope/vim-commentary.git plugins/commentary
 git submodule add https://github.com/ap/vim-css-color.git plugins/css-color
 git submodule add https://github.com/kien/ctrlp.vim.git plugins/ctrlp
 git submodule add https://github.com/Raimondi/delimitMate plugins/delimitmate
-git submodule add https://github.com/tpope/vim-dispatch.git plugins/dispatch
+# git submodule add https://github.com/tpope/vim-dispatch.git plugins/dispatch
 git submodule add https://github.com/tpope/vim-endwise.git plugins/endwise
 git submodule add https://github.com/tpope/vim-eunuch.git plugins/eunuch
-git submodule add https://github.com/bogado/file-line.git plugins/file-line
-git submodule add https://github.com/vim-scripts/Flex-Development-Support.git plugins/flex
+# git submodule add https://github.com/bogado/file-line.git plugins/file-line
+# git submodule add https://github.com/vim-scripts/Flex-Development-Support.git plugins/flex
 git submodule add https://github.com/tpope/vim-fugitive.git plugins/fugitive
 git submodule add https://github.com/mattn/gist-vim.git plugins/gist
 git submodule add https://github.com/airblade/vim-gitgutter.git plugins/gitgutter
-git submodule add https://github.com/jamessan/vim-gnupg.git plugins/gnupg
-git submodule add https://github.com/Shutnik/jshint2.vim.git plugins/jshint2
+# git submodule add https://github.com/jamessan/vim-gnupg.git plugins/gnupg
+# git submodule add https://github.com/Shutnik/jshint2.vim.git plugins/jshint2
 git submodule add https://github.com/edsono/vim-matchit.git plugins/matchit
 git submodule add https://github.com/terryma/vim-multiple-cursors.git plugins/multiple-cursors
-git submodule add https://github.com/scrooloose/nerdcommenter.git plugins/nerdcommenter
+# git submodule add https://github.com/scrooloose/nerdcommenter.git plugins/nerdcommenter
 git submodule add https://github.com/scrooloose/nerdtree.git plugins/nerdtree
 git submodule add https://github.com/chrisbra/NrrwRgn.git plugins/NrrwRgn
-git submodule add https://github.com/tpope/vim-obsession.git plugins/obsession
+# git submodule add https://github.com/tpope/vim-obsession.git plugins/obsession
 git submodule add https://github.com/yssl/QFEnter.git plugins/qfenter
 git submodule add https://github.com/tpope/vim-rake.git plugins/rake
 git submodule add https://github.com/tpope/vim-repeat.git plugins/repeat
-git submodule add https://github.com/rafaelfranca/rtf_pygmentize.git plugins/rtf_pygmentize
-git submodule add https://github.com/ktvoelker/sbt-vim.git plugins/sbt
+# git submodule add https://github.com/rafaelfranca/rtf_pygmentize.git plugins/rtf_pygmentize
+# git submodule add https://github.com/ktvoelker/sbt-vim.git plugins/sbt
 git submodule add https://github.com/honza/vim-snippets.git plugins/snippets
 git submodule add https://github.com/tpope/vim-speeddating.git plugins/speeddating
 git submodule add https://github.com/tpope/vim-surround.git plugins/surround
 git submodule add https://github.com/scrooloose/syntastic.git plugins/syntastic
-git submodule add https://github.com/majutsushi/tagbar.git plugins/tagbar
-git submodule add https://github.com/vim-scripts/taglist.vim.git plugins/taglist
-git submodule add https://github.com/tpope/vim-tbone.git plugins/tbone
+# git submodule add https://github.com/majutsushi/tagbar.git plugins/tagbar
+# git submodule add https://github.com/vim-scripts/taglist.vim.git plugins/taglist
+# git submodule add https://github.com/tpope/vim-tbone.git plugins/tbone
 git submodule add https://github.com/kana/vim-textobj-entire.git plugins/textobj-entire
 git submodule add https://github.com/kana/vim-textobj-function.git plugins/textobj-function
 git submodule add https://github.com/t9md/vim-textobj-function-ruby.git plugins/textobj-function-ruby
@@ -56,27 +56,27 @@ git submodule add https://github.com/mattn/webapi-vim.git plugins/webapi
 git submodule add https://github.com/regedarek/ZoomWin.git plugins/zoomwin
 
 # Langs
-git submodule add https://github.com/vim-scripts/Arduino-syntax-file.git langs/arduino
-git submodule add https://github.com/guns/vim-clojure-static.git langs/clojure
+# git submodule add https://github.com/vim-scripts/Arduino-syntax-file.git langs/arduino
+# git submodule add https://github.com/guns/vim-clojure-static.git langs/clojure
 git submodule add https://github.com/kchmck/vim-coffee-script.git langs/coffeescript
 git submodule add https://github.com/vim-scripts/csv.vim.git langs/csv
 git submodule add https://github.com/tpope/vim-cucumber.git langs/cucumber
-git submodule add https://github.com/elixir-lang/vim-elixir.git langs/elixir
-git submodule add https://github.com/jimenezrick/vimerl.git langs/erlang
+# git submodule add https://github.com/elixir-lang/vim-elixir.git langs/elixir
+# git submodule add https://github.com/jimenezrick/vimerl.git langs/erlang
 git submodule add https://github.com/tpope/vim-git.git langs/git
 git submodule add https://github.com/fatih/vim-go.git langs/golang
 git submodule add https://github.com/tpope/vim-haml.git langs/haml
-git submodule add https://github.com/wlangstroth/vim-haskell.git langs/haskell
+# git submodule add https://github.com/wlangstroth/vim-haskell.git langs/haskell
 git submodule add https://github.com/othree/html5.vim.git langs/html5
 git submodule add https://github.com/pangloss/vim-javascript.git langs/javascript
 git submodule add https://github.com/groenewege/vim-less.git langs/less
-git submodule add https://github.com/tpope/vim-liquid.git langs/liquid
+# git submodule add https://github.com/tpope/vim-liquid.git langs/liquid
 git submodule add https://github.com/tpope/vim-markdown.git langs/markdown
 git submodule add https://github.com/sunaku/vim-ruby-minitest.git langs/minitest
 git submodule add https://github.com/mustache/vim-mustache-handlebars.git langs/mustache-handlebars
 git submodule add https://github.com/mmalecki/vim-node.js.git langs/nodejs
-git submodule add https://github.com/ajf/puppet-vim.git langs/puppet
-git submodule add https://github.com/jayferd/ragel.vim.git langs/ragel
+# git submodule add https://github.com/ajf/puppet-vim.git langs/puppet
+# git submodule add https://github.com/jayferd/ragel.vim.git langs/ragel
 git submodule add https://github.com/tpope/vim-rails.git langs/rails
 git submodule add https://github.com/depuracao/vim-rdoc.git langs/rdoc
 git submodule add https://github.com/skwp/vim-rspec.git langs/rspec
@@ -84,5 +84,5 @@ git submodule add https://github.com/vim-ruby/vim-ruby.git langs/ruby
 git submodule add https://github.com/wting/rust.vim.git langs/rust
 git submodule add https://github.com/derekwyatt/vim-scala.git langs/scala
 git submodule add https://github.com/cakebaker/scss-syntax.vim.git langs/scss
-git submodule add https://github.com/slim-template/vim-slim.git langs/slim
-git submodule add https://github.com/timcharper/textile.vim.git langs/textile
+# git submodule add https://github.com/slim-template/vim-slim.git langs/slim
+# git submodule add https://github.com/timcharper/textile.vim.git langs/textile
